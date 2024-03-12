369bd8d8b27a29e79f442cb73b3f938c52c55367
808raf <102700868+808raf@users.noreply.github.com>
2024-03-11 14:36:13 -1000

    Add event listener and functions for each button.
    Each button now properly calculates the result.

--- script.js
+++ script.js
@@ -1,38 +1,98 @@
-const add = (int1, int2) => {
-    return int1 + int2
-}
+// Query Selectors
 
-const subtract = (int1, int2) => {
-    return int1 - int2
-}
+const numberBtn = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
 
-const multipy = (int1, int2) => {
-    return int1 * int2
-}
+const equalBtn = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
+const decimalBtn = document.querySelector(".decimal");
 
-const divide = (int1, int2) => {
-    return int1 / int2
-}
+const currentScreen = document.querySelector(".current-screen");
+const previousScreen = document.querySelector(".previous-screen");
 
-const operate = (sign, int1, int2) => {
-    switch (sign) {
-        case "-":
-            subtract(int1, int2);
-            break;
-        case "+":
-            add(int1, int2);
-            break;
-        case "X":
-            multipy(int1, int2);
-            break;
-        case "%":
-            divide(int1, int2);
-            break;
-        default:
-            break;
+// Event Listeners
+
+clearBtn.addEventListener("click", () => {
+    handleClear();
+})
+
+numberBtn.forEach((num) => {
+    num.addEventListener("click", (e) => {
+        handleNumber(e.target.innerText);
+        currentScreen.textContent = `${currentNum}`
+    })
+})
+
+operatorBtn.forEach((op) => {
+    op.addEventListener("click", (e) => {
+        if (currentNum !== "" && previousNum !== ""){
+            handleEqual();
+            previousNum = currentNum;
+            currentNum = "";
+            handleOperator(e.target.innerText);
+            currentScreen.textContent = previousNum;
+            previousScreen.textContent = `${previousNum} ${operator}`;
+        } else if(previousNum !== ""){
+            handleOperator(e.target.innerText);
+            previousScreen.textContent = `${previousNum} ${operator}`;
+        } else{
+            handleOperator(e.target.innerText);
+            previousScreen.textContent = `${currentNum} ${operator}`;
+            previousNum = currentNum;
+            currentNum = "";
+        }
+    })
+})
+
+equalBtn.addEventListener("click", () => {
+    if(currentNum !== "" && previousNum !== ""){
+        handleEqual();
+        previousNum = currentNum;
+        currentNum = "";
+        operator = "";
+        currentScreen.textContent = previousNum;
+        previousScreen.textContent = "";
     }
+})
+
+// Global Variables
+
+let operator = "";
+let currentNum = "";
+let previousNum = "";
+
+// Handle Functions
+
+const handleClear = () => {
+    operator = "";
+    currentNum = "";
+    previousNum = "";
+    currentScreen.textContent = "";
+    previousScreen.textContent = "";
 }
 
-const firstNumber = 0;
-const secondNumber = 0;
-const operator = "";
+const handleNumber = (num) => {
+    currentNum += num;
+}
+
+const handleOperator = (op) => {
+    operator = op;
+}
+
+const handleEqual = () => {
+    currentNum = Number(currentNum);
+    previousNum = Number(previousNum);
+
+    if(operator === "+"){
+        currentNum += previousNum;
+    } else if (operator === "-"){
+        currentNum = previousNum - currentNum;
+    } else if (operator === "*"){
+        currentNum *= previousNum
+    } else if (operator === "/"){
+        currentNum = previousNum / currentNum;
+    }
+
+    currentNum = currentNum.toString()
+    previousNum = previousNum.toString()
+}
